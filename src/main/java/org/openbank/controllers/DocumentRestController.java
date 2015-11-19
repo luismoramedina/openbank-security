5c9be6d4de171c0cf9bb4b8c4d1d5f03ea958884
luismoramedina <luismoramedina@gmail.com>
2015-11-19 12:59:38 +0100

    controller returns a base 64 file

--- src/main/java/org/openbank/controllers/DocumentRestController.java
+++ src/main/java/org/openbank/controllers/DocumentRestController.java
@@ -1,8 +1,11 @@
 package org.openbank.controllers;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
 /**
@@ -10,9 +13,24 @@ import java.util.Date;
  */
 @RestController
 public class DocumentRestController {
+
+    private static String base64file;
+
+    static {
+        InputStream resourceAsStream = DocumentRestController.class.getResourceAsStream("/docs/a-file-to-sign.txt");
+        try {
+            byte[] b = new byte[resourceAsStream.available()];
+            resourceAsStream.read(b);
+            resourceAsStream.close();
+            base64file = Base64.encodeBase64String(b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @RequestMapping(value = "/document", produces = "application/json")
     public String doSomething() {
         String time = new Date().toString();
-        return "{\"data\" : \"this is the " + time + "\"}";
+//        return "{\"data\" : \"this is the " + time + "\"}";
+        return "{\"data\" : \"" + base64file + "\"}";
     }
 }
