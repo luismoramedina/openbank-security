cf49c570f35cada6a21b2253770a2183df195e39
luismoramedina <luismoramedina@gmail.com>
2015-11-19 12:17:50 +0100

    first commit, app skeleton

---
+++ src/test/java/org/openbank/OpenbankSecurityBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package org.openbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OpenbankSecurityBackendApplication.class)
+public class OpenbankSecurityBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
