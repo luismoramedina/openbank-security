da7f3fc3784f028c338a2cc5a79c3eba84932922
rpastor <rodrigo.pastor@gmail.com>
2015-11-19 15:26:22 +0100

    endpoint subir archivo "/uploadFile"

--- src/main/java/org/openbank/controllers/DocumentRestController.java
+++ src/main/java/org/openbank/controllers/DocumentRestController.java
@@ -1,11 +1,10 @@
 package org.openbank.controllers;
 
 import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Date;
 
 /**
@@ -27,10 +26,52 @@ public class DocumentRestController {
             e.printStackTrace();
         }
     }
+
     @RequestMapping(value = "/document", produces = "application/json")
-    public String doSomething() {
+    public String doSomething(@RequestParam ("id") String documentId) {
         String time = new Date().toString();
 //        return "{\"data\" : \"this is the " + time + "\"}";
-        return "{\"data\" : \"" + base64file + "\"}";
+
+        return "{\"data\" : \"" + base64file + "\", \"id\" : \"" + documentId + "\"}";
+    }
+
+    /**
+     * Upload single file
+     */
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFileHandler(@RequestParam("name") String name,
+                             @RequestParam("file")
+                             MultipartFile file) {
+
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                                                   + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+
+                System.out.println("Server File Location="
+                                    + serverFile.getAbsolutePath());
+
+                return "You successfully uploaded file=" + name;
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name
+                    + " because the file was empty.";
+        }
     }
 }
