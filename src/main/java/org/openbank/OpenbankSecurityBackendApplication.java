c9dfa84a24504b3d69477899121bb9c88856a2ae
luismoramedina <luismoramedina@gmail.com>
2015-11-20 07:00:47 +0100

    springboot with war packaging

--- src/main/java/org/openbank/OpenbankSecurityBackendApplication.java
+++ src/main/java/org/openbank/OpenbankSecurityBackendApplication.java
@@ -2,9 +2,16 @@ package org.openbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OpenbankSecurityBackendApplication {
+public class OpenbankSecurityBackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OpenbankSecurityBackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OpenbankSecurityBackendApplication.class, args);
